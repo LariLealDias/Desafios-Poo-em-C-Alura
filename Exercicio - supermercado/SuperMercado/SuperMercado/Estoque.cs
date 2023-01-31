2777440ca0dd178db265083ac429fab791db819b
Lari <larileal@hotmail.com.br>
2023-01-31 17:48:48 -0300

    Desafio do curso 1

---
+++ Exercicio - supermercado/SuperMercado/SuperMercado/Estoque.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMercado
+{
+    public class Estoque
+    {
+        public string nome;
+        public string tipo;
+        public string fabricante;
+        public string Codigo;
+        public double valor;
+        public double quantidade;
+    }
+
+    public class Aluno
+    {
+        public string nome;
+        public string RA;
+        private int idade { get; set; }
+
+        public Aluno(string nome, string RA, int idade)
+        {
+            this.nome = nome;
+            this.RA = RA;
+            this.idade = idade;   
+        }
+
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value; }
+        }
+        //metodo
+        //public int VerificarAnoNascimento()
+        //{
+        //    int anoNascimento = idade - 2023;
+        //    return anoNascimento;
+        //}
+
+        //public void mesagemAnoNascimento()
+        //{
+        //    Console.WriteLine($"O {nome}, nasceu em {VerificarAnoNascimento()}");
+        //}
+    }
+
+}
---
+++ Exercicio - supermercado/SuperMercado/SuperMercado/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using SuperMercado;
+
+Estoque produto1 = new Estoque();
+
+produto1.nome = "bolacha chocolate";
+produto1.tipo = "alimenticio";
+produto1.fabricante = "recreios";
+produto1.quantidade = 10;
+produto1.Codigo = "193475-000";
+produto1.valor = 2.50;
+
+Console.WriteLine($"informações do produto1: nome - {produto1.nome}, tipo - {produto1.tipo}, codigo - {produto1.Codigo}");
+Console.WriteLine();
+
+
+//Aluno
+Aluno aluno1 = new Aluno("Fulano S", "12334-xa", 10);
+//aluno1.nome = "Fulano Silva";
+//aluno1.RA = "12334-xa";
+//aluno1.idade = 10;
+
+Console.WriteLine($"{aluno1.nome}");
+
+aluno1.Nome = "Fulano";
+//visualizar/get
+Console.WriteLine(aluno1.Nome);
+
+//Console.WriteLine($"O {aluno1.nome} nasceu em {aluno1.VerificarAnoNascimento()}");
+//aluno1.mesagemAnoNascimento();
+
+
+
+
+
+
+
+
+
+
+
+
